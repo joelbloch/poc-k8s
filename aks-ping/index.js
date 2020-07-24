52518319ded5d84b69d5a31eac328cfb5c5c46f8
joelbloch <joel.bloch@gmail.com>
2020-07-24 14:42:21 +0200

    Adding very basic test for AKS

---
+++ aks-ping/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get('/ping', (req, res) => {    
+    res.end('pong');
+});
+
+router.get('/*', (req, res) => {    
+    res.end(req.originalUrl);
+});
+
+app.use('/', router);
+
+app.listen(8080, () => {
+    console.log("aks-ping started on PORT " + 8080);
+});
