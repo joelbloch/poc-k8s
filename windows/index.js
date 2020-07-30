5dc18d23da006038604d956e5181ecfd511936e9
joelbloch <joel.bloch@gmail.com>
2020-07-30 14:23:58 +0200

    Adding access to external db

--- windows/index.js
+++ windows/index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
-const port = 3000;
-const appName = "Repeater";
+const pg = require('pg')
 
+const router = express.Router();
+
+const configPath = process.argv.slice(2)[0];
+var config = require(configPath);
+
+const client = new pg.Client({
+    user: config.db.user,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port,
+  });
+
+client.connect();
+client.query('SELECT NOW()', (err, res) => {
+    console.log("connection to Db");
+    if(err) {
+        console.log("there was an error with db query" + err.stack);
+    } else {
+        console.log("Here is the db response " + res);
+    }    
+    //client.end();
+})
 let counter = 0;
 
-router.get('/ping', (req, res) => {        
+
+router.get('/ping', (req, res) => {
     console.log(req.originalUrl);
-    res.end('pong from Windows Container');    
+    res.end('pong from Windows container');
+})
+
+router.get('/db', (req, res) => {
+    console.log(req.originalUrl);
+
+    client.query('SELECT NOW() as now')
+    .then(respdb => res.end("Here is the db response " + respdb.rows[0]))
+    .catch(err => res.end("there was an error with db query" + err.stack));
 })
 
 router.get('/counter', (req, res) => {        
@@ -24,6 +54,6 @@ router.get('/*', (req, res) => {
 
 app.use('/', router);
 
-app.listen(port, () => {
-    console.log(appName + " started on PORT " + port);
+app.listen(config.port, () => {
+    console.log(config.appName + " started on PORT " + config.port);
 });
