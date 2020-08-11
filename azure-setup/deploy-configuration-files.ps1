6603e0fd0a365aa2b4374cc539de78e96117008e
joelbloch <joel.bloch@gmail.com>
2020-08-11 15:29:36 +0200

    ...

--- azure-setup/deploy-configuration-files.ps1
+++ azure-setup/deploy-configuration-files.ps1
@@ -1,13 +1,10 @@
 $azConfig = Get-Content -Path "./azure-poc.config.json" | ConvertFrom-Json
 $storageName = $azConfig.filestorage.name
 $shareName = $azConfig.filestorage.fileshare
-
 $credentialFile = $azConfig.filestorage.generatedfilename
 $credentials = Get-Content -Path $credentialFile | ConvertFrom-Json
 
 az storage file upload-batch `
-    --account-name $storageName `
-    --account-key $credentials.password `
-    --share-name $shareName `
+    --connection-string $credentials.connectionstring `
     --source "../app/config/azure" `
-    --destination .
+    --destination ./$shareName
