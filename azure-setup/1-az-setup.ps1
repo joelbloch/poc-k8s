91830288e02d5fd3f0ef5d96c60c9a74c60bf73f
joelbloch <joel.bloch@gmail.com>
2020-08-11 14:34:03 +0200

    Delete az-login from main script, as it is supposed to be executed from azure cloud shell

--- azure-setup/1-az-setup.ps1
+++ azure-setup/1-az-setup.ps1
@@ -1,7 +1,5 @@
 $azConfig = Get-Content -Path ".\azure-poc.config.json" | ConvertFrom-Json
 
-. ./az-login.ps1
-
 #Create Resource Group
 Write-Host "Creating Resource Group $azConfig.group.name"
 az group create  --name  $azConfig.group.name `
