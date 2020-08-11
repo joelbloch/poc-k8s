08795ebe7365b7a12dfc2c7065091ee3aaf8ad1e
joelbloch <joel.bloch@gmail.com>
2020-08-11 16:45:50 +0200

    Switching from upload-batch to upload

--- azure-setup/deploy-configuration-files.ps1
+++ azure-setup/deploy-configuration-files.ps1
@@ -1,12 +1,15 @@
-$azConfig = Get-Content -Path "./azure-poc.config.json" | ConvertFrom-Json
+c$azConfig = Get-Content -Path "./azure-poc.config.json" | ConvertFrom-Json
 $storageName = $azConfig.filestorage.name
 $shareName = $azConfig.filestorage.fileshare
 $credentialFile = $azConfig.filestorage.generatedfilename
 $credentials = Get-Content -Path $credentialFile | ConvertFrom-Json
+$accountKey = $credentials.password 
 $connectionString = $credentials.connectionstring
 
 Write-Host "Uploading configuration file to Azure Fileshare with connection string $connectionString"
-az storage file upload-batch `
-    --connection-string $credentials.connectionstring `
-    --source "../app/config/azure" `
-    --destination ./$shareName
+
+Get-ChildItem -Path ..\app\config\azure\*.json -Name |  `
+    az storage file upload `
+        --share-name $shareName `
+        --source ../app/config/azure/$_ `
+        --connection-string $connectionString
