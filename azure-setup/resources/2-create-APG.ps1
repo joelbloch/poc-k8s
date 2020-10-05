6402faf6988459a2d4156ed11653d554157937ed
joelbloch <joel.bloch@gmail.com>
2020-10-05 09:07:50 +0200

    correction in creation of APG script

--- azure-setup/resources/2-create-APG.ps1
+++ azure-setup/resources/2-create-APG.ps1
@@ -9,24 +9,21 @@ az postgres server create --resource-group $azConfig.group.name `
                           --admin-password $azConfig.db.admin.password `
                           --sku-name $azConfig.db.sku
 
-Write-Host "Disabling SSL connection for the POC"
-az postgres server update --name $azConfig.db.database `
-                        --resource-group $azConfig.group.name `
-                        --ssl-enforcement Disabled
-
-$DbName = $azConfig.db.name
-$ServerName = $DbName +".postgres.database.azure.com"
-
-Write-Host "Allowing all incoming requests from Azure"
-az postgres server firewall-rule create --resource-group $DbName `
-                                        --server-name $ServerName `
-                                        --name AllowAllAzureIps `
-                                        --start-ip-address 0.0.0.0 `
-                                        --end-ip-address 0.0.0.0 `
-
 Write-Host "Creating Azure Postgres Database $azConfig.db.database on Server $azConfig.db.name"
 az postgres db create --name $azConfig.db.database `
                       --resource-group $azConfig.group.name `
                       --server-name $azConfig.db.name
+
+Write-Host "Disabling SSL connection for the POC"
+az postgres server update --name $azConfig.db.name `
+                        --resource-group $azConfig.group.name `
+                        --ssl-enforcement Disabled
+
+Write-Host "Allowing all incoming requests from Azure"
+az postgres server firewall-rule create --resource-group $azConfig.db.name `
+                                        --server-name $ServerName `
+                                        --name AllowAllAzureIps `
+                                        --start-ip-address 0.0.0.0 `
+                                        --end-ip-address 0.0.0.0 `
 
 
